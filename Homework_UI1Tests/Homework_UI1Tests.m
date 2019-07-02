c14eb8b2c36df16478b8691db37966fd81e64cc5
Natali-QA <natagorb343@gmail.com>
2019-07-02 23:29:33 +0300

    fix

--- Homework_UI1Tests/Homework_UI1Tests.m
+++ Homework_UI1Tests/Homework_UI1Tests.m
@@ -27,14 +27,12 @@
 
 - (void)testExample {
     // This is an example of a functional test case.
-    XCTAssert(YES, @"Fail");
+    XCTAssert(NODEV, @"Fail");
 }
 
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+    
 }
 
 @end
